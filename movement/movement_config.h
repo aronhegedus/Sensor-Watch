fabd413453c70257f8abf5a9f55cdd40e05fd367
aronhegedus <hegedusaron@yahoo.com>
2023-09-28 00:11:10 +0100

    some comment

--- movement/movement_config.h
+++ movement/movement_config.h
@@ -30,7 +30,7 @@
 const watch_face_t watch_faces[] = {
     simple_clock_face,
     stopwatch_face,
-    set_time_face,
+    set_time_face, // this is causing me issues, I can't scroll past it.
     preferences_face,
     sea_shanty_face,
 };
